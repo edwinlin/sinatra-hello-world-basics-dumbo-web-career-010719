ded7b5b1832a00b5e48c68144cd2a948e0f5e22f
edwinlin <edwinlin@gmail.com>
2019-01-28 00:13:13 -0500

    Done.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 source "https://rubygems.org"
 
 # gem "rails"
+gem 'nokogiri'
 
 gem 'sinatra'
     gem 'rake'
--- app.rb
+++ app.rb
@@ -3,4 +3,11 @@ class App < Sinatra::Base
   # Define your GET '/' route below and respond with "Hello, World!"
 
 
+  get '/' do
+  	resp = Rack::Response.new
+
+  	# resp.status = 200
+  	resp.write "Hello, World!"
+  end
+
 end
